661b03806ee40811c0a0d4843a005df6838085ca
Administrator <Administrator@WIN-UT0SMI9LMU6>
2024-09-29 13:17:46 +0800

    RTL patch

--- patchrtl.cmd
+++ patchrtl.cmd
@@ -1,19 +1,20 @@
 @echo off
+cd /d %~dp0
+echo powershell -Command ^"Get-ChildItem -Recurse -Filter '*.vcxproj' ^| ForEach-Object { ^
+    $filePath = $_.FullName;^
+    Write-Host 'Processing' $filePath; ^
+    (Get-Content $filePath) -replace 'MultiThreadedDebugDLL', 'MultiThreadedDebug' ` ^
+                               -replace 'MultiThreadedDLL', 'MultiThreaded' ` ^
+                               ^| Set-Content $filePath ^
+}^"
  
-powershell -Command "Get-ChildItem -Recurse -Filter '*.vcxproj' | ForEach-Object {
-    $filePath = $_.FullName
-    Write-Host 'Processing' $filePath
-    (Get-Content $filePath) -replace 'MultiThreadedDebugDLL', 'MultiThreadedDebug' `
-                               -replace 'MultiThreadedDLL', 'MultiThreaded' `
-                               | Set-Content $filePath
-}"
+echo powershell -Command ^"Get-ChildItem -Recurse -Filter '*.vc' ^| ForEach-Object { ^
+    $filePath = $_.FullName; ^
+    Write-Host 'Processing' $filePath; ^
+    (Get-Content $filePath) -replace '/MDd', '/MTd' ` ^
+                               -replace '/MD', '/MT' ` ^
+                               -replace '-MDd', '-MTd' ` ^
+                               -replace '-MD', '-MT' ` ^
+                               ^| Set-Content $filePath ^
+}^"
  
-powershell -Command "Get-ChildItem -Recurse -Filter '*.vc' | ForEach-Object {
-    $filePath = $_.FullName
-    Write-Host 'Processing' $filePath
-    (Get-Content $filePath) -replace '/MDd', '/MTd' `
-                               -replace '/MD', '/MT' `
-                               -replace '-MDd', '-MTd' `
-                               -replace '-MD', '-MT' `
-                               | Set-Content $filePath
-}"
