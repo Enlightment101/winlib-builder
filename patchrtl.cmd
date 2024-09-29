072084b6ad83d89a8a91c354601a9906839c91f1
Administrator <Administrator@WIN-UT0SMI9LMU6>
2024-09-29 13:20:33 +0800

    RTL patch

--- patchrtl.cmd
+++ patchrtl.cmd
@@ -1,6 +1,6 @@
 @echo off
 cd /d %~dp0
-echo powershell -Command ^"Get-ChildItem -Recurse -Filter '*.vcxproj' ^| ForEach-Object { ^
+powershell -Command ^"Get-ChildItem -Recurse -Filter '*.vcxproj' ^| ForEach-Object { ^
     $filePath = $_.FullName;^
     Write-Host 'Processing' $filePath; ^
     (Get-Content $filePath) -replace 'MultiThreadedDebugDLL', 'MultiThreadedDebug' ` ^
@@ -8,7 +8,7 @@ echo powershell -Command ^"Get-ChildItem -Recurse -Filter '*.vcxproj' ^| ForEach
                                ^| Set-Content $filePath ^
 }^"
  
-echo powershell -Command ^"Get-ChildItem -Recurse -Filter '*.vc' ^| ForEach-Object { ^
+powershell -Command ^"Get-C hildItem -Recurse -Filter '*.vc' ^| ForEach-Object { ^
     $filePath = $_.FullName; ^
     Write-Host 'Processing' $filePath; ^
     (Get-Content $filePath) -replace '/MDd', '/MTd' ` ^
@@ -17,4 +17,4 @@ echo powershell -Command ^"Get-ChildItem -Recurse -Filter '*.vc' ^| ForEach-Obje
                                -replace '-MD', '-MT' ` ^
                                ^| Set-Content $filePath ^
 }^"
- 
+exit /b 0
